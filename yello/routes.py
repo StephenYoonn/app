1199733c48cc468a8021da03471a6e9708a58662
stephenyoonn <yoon.stephen123@gmail.com>
2024-04-18 13:50:13 -0400

    compression and encryption

--- yello/routes.py
+++ yello/routes.py
@@ -2,23 +2,47 @@
 
 from . import app
 from flask import request, jsonify
+from cryptography.fernet import Fernet
+import zlib
+import base64
 
+
+url_mapping = {}
+key = Fernet.generate_key()
+f = Fernet(key)
 
 @app.route('/encode', methods=['POST'])
-def encode(url):
-    print(url)
+def encode():
 
-    
-    newUrl = shortUrl(url)
-    
-    
-    return jsonify({"oldLink" : url, "encodedLink" : newUrl})
+    #json get input
+    data = request.get_json()
+    if 'url' in data:
+        url = data['url'] 
+    else:
+        return jsonify({"error": "URL not provided"}), 400
+
+    compressed = zlib.compress(url.encode('utf-8'))
+    encrypted =f.encrypt(compressed)
+    newUrl = base64.urlsafe_b64encode(encrypted).decode('utf-8')
+
+    return jsonify({"oldLink": url, "encodedLink": f"https://short.est/{newUrl}"})
 
 def shortUrl(url):
     newUrl = hash(url) % 1000
-    return f"short.est/{newUrl}"
+    return f"https://short.est/{newUrl}"
 
 @app.route('/decode', methods=['POST'])
 def decode():
-    # Your existing decode logic here
-    return jsonify(result="original_url_here")
+    data = request.get_json()
+    if 'url' in data:
+        url = data['url'] 
+    else:
+        return jsonify({"error": "URL not provided"}), 400
+    
+    newUrl = f.decrypt(url)
+    return jsonify({"oldLink": newUrl})
+    
+        
+    
+
+    
